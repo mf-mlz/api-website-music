71ac1be2aeeb0cc345ed362c0f41438829e7b453
“Fernanda <maryfermtzlb@gmail.com>
2024-09-18 17:20:51 -0600

    feat(SSL): index pem

--- index.js
+++ index.js
@@ -2,27 +2,37 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const cors = require('cors');
-const rateLimit = require('express-rate-limit');
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 const PORT = process.env.PORT || 5500;
 const videosRoute = require("./routes/videos");
 const imagesRoutes = require("./routes/images");
 const surveyRoutes = require("./routes/survey");
 const registerRoutes = require("./routes/register");
+const https = require('https');
+const fs = require('fs');
+
+/* SSL */
+const options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/thecognacmusic.com/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/thecognacmusic.com/fullchain.pem"),
+};
 
 /* Configurations */
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
   handler: (req, res) => {
-    return res.status(429).json({ type: "error", data: "Error en el Intervalo de Peticiones" });
+    return res
+      .status(429)
+      .json({ type: "error", data: "Error en el Intervalo de Peticiones" });
   },
   headers: true,
 });
 
 app.use(
   cors({
-    origin: process.env.HOST_FRONT.replace(/\/$/, ''),
+    origin: process.env.HOST_FRONT.replace(/\/$/, ""),
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
@@ -39,6 +49,6 @@ app.use("/images", imagesRoutes);
 app.use("/survey", surveyRoutes);
 app.use("/register", registerRoutes);
 
-app.listen(PORT, () => {
+https.createServer(options, app).listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${process.env.HOST_FRONT}`);
 });
